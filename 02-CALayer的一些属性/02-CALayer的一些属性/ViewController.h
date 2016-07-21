c07024d931818c335c8dca6937d0729f002e9d84
mukang <mukang.max@foxmail.com>
2016-07-21 14:44:12 +0800

    02-CALayer的一些属性
    创建 02-CALayer的一些属性 项目

---
+++ 02-CALayer的一些属性/02-CALayer的一些属性/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  02-CALayer的一些属性
+//
+//  Created by 穆康 on 16/7/21.
+//  Copyright © 2016年 穆康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 02-CALayer的一些属性/02-CALayer的一些属性Tests/_2_CALayer_____Tests.m
@@ -0,0 +1,39 @@
+//
+//  _2_CALayer_____Tests.m
+//  02-CALayer的一些属性Tests
+//
+//  Created by 穆康 on 16/7/21.
+//  Copyright © 2016年 穆康. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _2_CALayer_____Tests : XCTestCase
+
+@end
+
+@implementation _2_CALayer_____Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
