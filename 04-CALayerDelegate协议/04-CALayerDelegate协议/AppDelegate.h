80db9fd93ca1b4231b06f6b01b4843de4680e7b5
mukang <mukang.max@foxmail.com>
2016-07-21 15:52:33 +0800

    04-CALayerDelegate协议
    创建 04-CALayerDelegate协议 项目

---
+++ 04-CALayerDelegate协议/04-CALayerDelegate协议/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  04-CALayerDelegate协议
+//
+//  Created by 穆康 on 16/7/21.
+//  Copyright © 2016年 穆康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
