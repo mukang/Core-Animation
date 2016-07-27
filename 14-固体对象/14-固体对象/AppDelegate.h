783dccb93f938336896f20551bda400c5b6ee47f
mukang <mukang.max@foxmail.com>
2016-07-27 18:05:22 +0800

    14-固体对象

---
+++ 14-固体对象/14-固体对象/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  14-固体对象
+//
+//  Created by 穆康 on 16/7/26.
+//  Copyright © 2016年 穆康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 14-固体对象/14-固体对象/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  14-固体对象
+//
+//  Created by 穆康 on 16/7/26.
+//  Copyright © 2016年 穆康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
